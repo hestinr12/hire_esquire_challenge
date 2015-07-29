7a4f45e8fe1fd6b3bd6540681ae36813dbe09bbc
Ryan Hestin <ryAn@Ryans-MacBook-Pro.local>
2015-07-29 00:24:39 -0400

    initial commit, adds model classes (not implemented) and appropriate files

---
+++ time_keeper/models.py
@@ -0,0 +1,16 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class TimeEntry(models.Model):
+    # minutes_worked = models.PositiveSmallIntegerField()
+    # time_stamp = models.DateField()
+    pass
+
+
+class Job(models.Model):
+    pass
+    
